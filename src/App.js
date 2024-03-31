221ac6de23824e60d56020376a26910d6f78ebbd
Yugal2003 <yugalramesh6832@gmail.com>
2024-03-31 11:40:06 +0530

    Added Files

--- src/App.js
+++ src/App.js
@@ -1,24 +1,56 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import { getAuth, createUserWithEmailAndPassword,signInWithEmailAndPassword } from "firebase/auth";
+
 
 function App() {
+  const [email,setEmail] = useState();
+  const [password,setPassword] = useState();
+
+  const auth = getAuth();
+
+  async function signUp(){
+    try {
+      const dataSignUp = await createUserWithEmailAndPassword(auth,email,password);
+      alert("User Sign Up")
+    } 
+    catch (error) {
+      alert("User Already Present")
+    }
+  }
+
+  async function signIn(){
+    try {
+      const dataSignIn = await signInWithEmailAndPassword(auth,email,password);
+      alert("User Login Successfully")
+    } catch (error) {
+      alert("Sign Up First")
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <h1 className='h1tag'>Firebase Authentication</h1>
+    <div className='main'>
+      <div className='signup'>
+          <h2>User Sign Up</h2>
+          <div className='email_password'>
+              <input placeholder='Enter email' type='email' onChange={(e) => setEmail(e.target.value)}/><br></br>
+              <input placeholder='Enter password' type='text' onChange={(e) => setPassword(e.target.value)}/><br></br>
+              <button type='submit' onClick={signUp}>Sign Up</button>
+          </div>
+      </div>
+
+      <div className='signin'>
+          <h2>User Sign In</h2>
+          <div className='email_password'>
+              <input placeholder='Enter email' type='email' onChange={(e) => setEmail(e.target.value)}/><br></br>
+              <input placeholder='Enter password' type='text' onChange={(e) => setPassword(e.target.value)}/><br></br>
+              <button type='submit' onClick={signIn}>Sign In</button>
+          </div>
+      </div>
     </div>
+    </>
   );
 }
 
---
+++ src/firebase.js
@@ -0,0 +1,16 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBmXX5gxoY9LY6KAanp3Czan-9CDX5NM1k",
+  authDomain: "practice-project-5e284.firebaseapp.com",
+  projectId: "practice-project-5e284",
+  storageBucket: "practice-project-5e284.appspot.com",
+  messagingSenderId: "586532733022",
+  appId: "1:586532733022:web:c1337ad93f910f117074cf",
+  measurementId: "G-JWX4LNBPK6"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
